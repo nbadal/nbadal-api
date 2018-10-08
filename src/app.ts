3effad077e53f364869bcefea8327e4afcab5ea6
Nick Badal <nick.badal@razer.com>
2018-10-08 09:50:55 -0700

    Use TypeScript

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import {Request, Response} from "express-serve-static-core";
+import createError from "http-errors";
+import logger from "morgan";
+import path from "path";
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.config();
+    }
+
+    private config() {
+        this.express.set("views", path.join(__dirname, "views"));
+        this.express.set("view engine", "pug");
+
+        this.express.use(logger("dev"));
+        this.express.use(express.json());
+        this.express.use(express.urlencoded({extended: false}));
+        this.express.use(cookieParser());
+        this.express.use(express.static(path.join(__dirname, "public")));
+
+        this.express.get("/", (req, res) => {
+            res.redirect(301, "https://nbad.al");
+        });
+        this.express.get("/test.json", (req, res) => {
+            res.json({message: "hello json"});
+        });
+
+        this.express.use((req, res, next) => {
+            next(createError(404));
+        });
+        this.express.use((err: any, req: Request, res: Response) => {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.status = err.status;
+            res.locals.stack = this.express.get("env") === "development" ? err.stack : "";
+
+            // render the error page
+            res.status(err.status || 500);
+            res.render("error");
+        });
+    }
+}
+
+export default new App().express;
---
+++ src/server.ts
@@ -0,0 +1,73 @@
+import Debug from "debug";
+import http from "http";
+import express from "./app";
+
+const debug = Debug("nbadal-api:server");
+
+const port = normalizePort(process.env.PORT || "3000");
+express.set("port", port);
+
+const server = http.createServer(express);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val: string) {
+    const portNum = parseInt(val, 10);
+
+    if (isNaN(portNum)) {
+        // named pipe
+        return val;
+    }
+
+    if (portNum >= 0) {
+        // port number
+        return portNum;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: NodeJS.ErrnoException) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    const bind = typeof port === "string"
+        ? "Pipe " + port
+        : "Port " + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            debug("ERR: " + bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            debug("ERR: " + bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === "string"
+        ? "pipe " + addr
+        : "port " + addr.port;
+    debug("Listening on " + bind);
+}
