bb73714a53f53e6b60d0943f61da00ce195bf40e
Nick Badal <me@nbad.al>
2018-10-10 21:51:35 -0700

    Combine and reduce both in-progress and todo items in /trello response

--- src/trello.ts
+++ src/trello.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import Trello from "node-trello";
 import {BehaviorSubject, bindNodeCallback} from "rxjs";
-import {first, map, mergeMap} from "rxjs/operators";
+import {filter, map, mergeMap, reduce} from "rxjs/operators";
 
 const router = express.Router();
 let trelloApi: Trello;
@@ -20,13 +20,17 @@ try {
 
 function getTrelloCards() {
     const boardId = "0vBMcbdj";
-    const listName = "Todo";
+    const listNames = ["in progress", "todo"];
     const listArgs = {fields: "name", cards: "open", card_fields: "name"};
 
     return trelloGet(`/1/boards/${boardId}/lists`, listArgs).pipe(
         mergeMap((lists) => lists as any[]),
-        first((list) => list.name.toLowerCase() === listName.toLowerCase()),
-        map((list) => list.cards),
+        filter((list) => listNames.indexOf(list.name.toLowerCase()) >= 0),
+        reduce((listMap: any, list) => {
+            const formattedName = list.name.replace(" ", "-").toLowerCase();
+            listMap[formattedName] = list.cards;
+            return listMap;
+        }, {}),
     );
 }
 
@@ -41,8 +45,8 @@ router.get("/", (req, res, next) => {
             mergeMap(() => getTrelloCards()),
             map((cards) => JSON.stringify(cards)),
             map((cards) => `data: ${cards}\n\n`),
-        ).subscribe(res.write);
-        req.on("close", trelloSub.unsubscribe);
+        ).subscribe((msg) => res.write(msg));
+        req.on("close", () => trelloSub.unsubscribe());
     } else {
         next();
     }
