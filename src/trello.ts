fc0f9441bac3e00038472e7044ce95fab4a43c1e
Nick Badal <me@nbad.al>
2018-10-25 12:08:29 -0700

    Remove old Overlay APIs, add SSE to new approach

--- src/trello.ts
+++ src/trello.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import Trello from "node-trello";
 import {OAuth} from "oauth";
-import {BehaviorSubject, bindNodeCallback, Observable, Subscriber} from "rxjs";
-import {filter, map, mergeMap, reduce} from "rxjs/operators";
+import {BehaviorSubject, Observable, Subject, Subscriber} from "rxjs";
+import {map, mergeMap} from "rxjs/operators";
 import firebase from "./firebase";
 
 let trelloKey: string;
@@ -16,11 +15,6 @@ try {
 }
 
 const router = express.Router();
-const trelloApi = new Trello(trelloKey, trelloSecret);
-const trelloGet = bindNodeCallback((uri: string, args: object,
-                                    callback: (err: Error, body: any) => void) => {
-    trelloApi.get(uri, args, callback);
-});
 const trelloUpdateSubject = new BehaviorSubject(true);
 
 const appName = "Trello Twitch Overlay";
@@ -30,22 +24,6 @@ const authorizeURL = "https://trello.com/1/OAuthAuthorizeToken";
 const loginCallback = `http://localhost:3000/trello/callback`;
 const trelloAuth = new OAuth(requestURL, accessURL, trelloKey, trelloSecret,
     "1.0A", loginCallback, "HMAC-SHA1");
-
-function getTrelloCards() {
-    const boardId = "0vBMcbdj";
-    const listNames = ["in progress", "todo"];
-    const listArgs = {fields: "name", cards: "open", card_fields: "name"};
-
-    return trelloGet(`/1/boards/${boardId}/lists`, listArgs).pipe(
-        mergeMap((lists) => lists as any[]),
-        filter((list) => listNames.indexOf(list.name.toLowerCase()) >= 0),
-        reduce((listArray: any, list) => {
-            const formattedName = list.name.replace(" ", "-").toLowerCase();
-            listArray.push({name: formattedName, cards: list.cards});
-            return listArray;
-        }, []),
-    );
-}
 
 function bindOauthRequest(url: string, method: string, token: string, secret: string): Observable<any> {
     return Observable.create((observer: Subscriber<any>) => {
@@ -64,54 +42,57 @@ function bindOauthRequest(url: string, method: string, token: string, secret: st
     });
 }
 
+function getOverlay(id: string): Observable<any> {
+    const resultSubject = new Subject();
+    firebase.firestore().doc(`overlays/${id}`).get().then((overlayDoc) => {
+        const overlayData = overlayDoc.data();
+        if (!overlayData) {
+            resultSubject.error({error: "Overlay not found."});
+            return;
+        }
+
+        const userId = overlayData.user;
+
+        firebase.firestore().doc(`users/${userId}`).get().then((userDoc) => {
+            const userData = userDoc.data();
+            if (!userData) {
+                resultSubject.error({error: "User not found."});
+                return;
+            }
+
+            const auth = userData.trelloAuth;
+
+            getOverlay2(overlayData, auth).subscribe(resultSubject);
+        });
+    });
+    return resultSubject;
+}
+
+function getOverlay2(overlayData: any, trelloCreds: any): Observable<any> {
+    return bindOauthRequest(`https://api.trello.com/1/boards/${overlayData.board}/lists`
+        + `?fields=name&cards=open&card_fields=name`,
+        "GET", trelloCreds.token, trelloCreds.secret)
+        .pipe(map((boardLists: any[]) => {
+            const listData = boardLists
+                .filter((list) => overlayData.lists.indexOf(list.id) >= 0)
+                .map((list) => {
+                    const formattedName = list.name.replace(" ", "-").toLowerCase();
+                    return {name: formattedName, cards: list.cards};
+                });
+
+            return {
+                lists: listData,
+                title: overlayData.title,
+            };
+        }));
+}
+
 router.get("/:id", (req, res, next) => {
     if (!req.params.id) {
         return next();
     }
 
     const overlayId = req.params.id;
-    firebase.firestore().doc(`overlays/${overlayId}`).get().then((overlayDoc) => {
-        const overlayData = overlayDoc.data();
-        if (!overlayData) {
-            res.sendStatus(400);
-            return;
-        }
-
-        const userId = overlayData.user;
-        const board = overlayData.board;
-        const lists = overlayData.lists as string[];
-        const title = overlayData.title;
-
-        firebase.firestore().doc(`users/${userId}`).get().then((userDoc) => {
-            const userData = userDoc.data();
-            if (!userData) {
-                res.sendStatus(404);
-                return;
-            }
-
-            const auth = userData.trelloAuth;
-            bindOauthRequest(`https://api.trello.com/1/boards/${board}/lists`
-                + `?fields=name&cards=open&card_fields=name`,
-                "GET", auth.token, auth.secret).subscribe((boardLists: any[]) => {
-                const listData = boardLists
-                    .filter((list) => lists.indexOf(list.id) >= 0)
-                    .map((list) => {
-                        const formattedName = list.name.replace(" ", "-").toLowerCase();
-                        return {name: formattedName, cards: list.cards};
-                    });
-
-                const result = {
-                    lists: listData,
-                    title,
-                };
-
-                res.json(result);
-            });
-        });
-    });
-});
-
-router.get("/", (req, res, next) => {
     if ("text/event-stream" === req.header("accept")) {
         res.writeHead(200, {
             "Cache-Control": "no-cache",
@@ -119,7 +100,7 @@ router.get("/", (req, res, next) => {
             "Content-Type": "text/event-stream",
         });
         const trelloSub = trelloUpdateSubject.pipe(
-            mergeMap(() => getTrelloCards()),
+            mergeMap(() => getOverlay(overlayId)),
             map((cards) => JSON.stringify(cards)),
             map((cards) => `data: ${cards}\n\n`),
         ).subscribe((msg) => res.write(msg));
@@ -129,13 +110,20 @@ router.get("/", (req, res, next) => {
     }
 });
 
-router.get("/", (req, res) => {
-    getTrelloCards().subscribe((result) => {
-        res.json(result);
-    }, (err: Error) => {
-        res.json({error: err});
+router.get("/:id", (req, res, next) => {
+    if (!req.params.id) {
+        return next();
+    }
+
+    const overlayId = req.params.id;
+    getOverlay(overlayId).subscribe((overlay) => {
+        res.json(overlay);
+    }, (error) => {
+        res.json(error);
     });
+
 });
+
 router.post("/", (req, res) => {
     // console.log("Got Trello POST: " + JSON.stringify(req.body));
     trelloUpdateSubject.next(true);
